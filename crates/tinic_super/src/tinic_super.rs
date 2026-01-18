aaabb047a50eefd36c17901805606d15a25ab1e5
Xsimple1010 <ader78@gmail.com>
2026-01-18 20:49:37 -0300

    renomeia core_info_helper para info_helper

--- crates/tinic_super/src/tinic_super.rs
+++ crates/tinic_super/src/tinic_super.rs
@@ -1,3 +1,4 @@
+use crate::cores::CoreHelper;
 use crate::event::TinicSuperEventListener;
 use crate::infos::helper::InfoHelper;
 use crate::rdb_manager::helper::RdbManager;
@@ -6,19 +7,24 @@ use std::sync::Arc;
 
 pub struct TinicSuper {
     pub retro_paths: RetroPaths,
-    pub core_info_helper: InfoHelper,
+    pub info_helper: InfoHelper,
     pub rdb_helper: RdbManager,
+    pub core_helper: CoreHelper,
 }
 
 impl TinicSuper {
     pub fn new(retro_paths: RetroPaths, event_listener: Arc<dyn TinicSuperEventListener>) -> Self {
         Self {
-            core_info_helper: InfoHelper {
+            info_helper: InfoHelper {
                 event_listener: event_listener.clone(),
                 retro_paths: retro_paths.clone(),
             },
             rdb_helper: RdbManager {
                 retro_path: retro_paths.clone(),
+                event_listener: event_listener.clone(),
+            },
+            core_helper: CoreHelper {
+                retro_paths: retro_paths.clone(),
                 event_listener: event_listener.clone(),
             },
             retro_paths,
