a34da5a74e703485980519af66b4c75cf7c2a506
Xsimple1010 <95971568+Xsimple1010@users.noreply.github.com>
2026-02-07 19:51:21 -0300

    separa o WindowEvents em um arquivo dedicado

--- crates/tinic_ipc/src/main.rs
+++ crates/tinic_ipc/src/main.rs
@@ -3,50 +3,13 @@ mod constants;
 mod device_listener;
 mod game_loop;
 mod io;
+mod window_event_listener;
 
-use crate::app_state::{AppState, AppStateHandle};
 use crate::device_listener::DeviceEventHandle;
 use crate::game_loop::game_loop;
 use crate::io::stdin_reader::StdinReader;
-use crate::io::stdout_writer::StdoutWriter;
-use std::sync::atomic::Ordering;
-use tinic::{ErrorHandle, GameState, SaveStateInfo, Tinic, WindowListener, WindowState};
-
-struct WindowEvents {
-    app_state: AppStateHandle,
-}
-
-impl WindowListener for WindowEvents {
-    fn window_state_change(&self, state: WindowState) {
-        let _ = StdoutWriter::window_state_change(state);
-    }
-
-    fn game_state_change(&self, state: GameState) {
-        match &state {
-            GameState::Closed => {
-                self.app_state.game_loaded.store(false, Ordering::SeqCst);
-            }
-            GameState::Running => {
-                self.app_state.game_loaded.store(true, Ordering::SeqCst);
-            }
-            _ => {}
-        };
-
-        let _ = StdoutWriter::game_state_change(state);
-    }
-
-    fn save_state_result(&self, info: SaveStateInfo) {
-        let _ = StdoutWriter::save_state_result(info);
-    }
-
-    fn load_state_result(&self, suss: bool) {
-        let _ = StdoutWriter::load_state_result(suss);
-    }
-
-    fn keyboard_state(&self, has_using: bool) {
-        let _ = StdoutWriter::keyboard_state(has_using);
-    }
-}
+use crate::{app_state::AppState, window_event_listener::WindowEvents};
+use tinic::{ErrorHandle, Tinic};
 
 fn main() -> Result<(), ErrorHandle> {
     // tinic config
